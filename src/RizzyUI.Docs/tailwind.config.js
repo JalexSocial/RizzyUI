54683efa6d8073e5bf5782aa507be191d8e378ae
Michael Tanczos <tanczosm@eastonsd.org>
2024-11-14 10:46:11 -0500

    chore: added docs project

---
+++ src/RizzyUI.Docs/tailwind.config.js
@@ -0,0 +1,37 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
+const plugin = require('tailwindcss/plugin');
+
+import aspectRatio from "@tailwindcss/aspect-ratio";
+import forms from "@tailwindcss/forms";
+import typography from "@tailwindcss/typography";
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./Components/**/*.{razor,html,cshtml,js}",
+        './wwwroot/js/rizzyui.js'
+    ],
+    darkMode: 'class',
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Nunito Sans', 'open sans', ...defaultTheme.fontFamily.sans],
+            }
+        },
+    },
+    plugins: [
+        aspectRatio,
+        forms,
+        typography,
+        plugin(function ({ addVariant }) {
+            addVariant('htmx-settling', ['&.htmx-settling', '.htmx-settling &']);
+            addVariant('htmx-request', ['&.htmx-request', '.htmx-request &']);
+            addVariant('htmx-swapping', ['&.htmx-swapping', '.htmx-swapping &']);
+            addVariant('htmx-added', ['&.htmx-added', '.htmx-added &']);
+        }),
+    ],
+};
+
+
+
