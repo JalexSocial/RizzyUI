fabe2ae149f808fd3af9e0901b8b3b941776f93b
Michael Tanczos <mike@logic-gate.com>
2024-11-17 19:49:27 -0500

    feat: update UI components to include tailwind class merging
    - Updated the `RizzyUI.SidebarLinkItem.Icon` documentation to reflect changes in icon usage.
    - Added new CSS variables for color schemes and updated existing classes to use these variables.
    - Modified the `Button.razor` component, improving attribute handling and group functionality.
    - Created a new file `ServiceCollectionExtensions.cs` for adding RizzyUI services to IServiceCollection.
    - Updated `package-lock.json`, adding a new dependency and removing unnecessary dev flags.

---
+++ src/RizzyUI/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using TailwindMerge.Extensions;
+
+namespace RizzyUI;
+
+public static class ServiceCollectionExtensions
+{
+	/// <summary>
+	/// Add RizzyUI services to <see cref="IServiceCollection"/>.
+	/// </summary>
+	/// <param name="services">The <see cref="IServiceCollection"/>.</param>
+	public static void AddRizzyUIServices(this IServiceCollection services)
+	{
+		services.AddTailwindMerge();
+	}
+}
--- src/RizzyUI/RizzyComponent.cs
+++ src/RizzyUI/RizzyComponent.cs
@@ -1,26 +1,31 @@
 ﻿using Microsoft.AspNetCore.Components;
+using System.Diagnostics.CodeAnalysis;
+using TailwindMerge;
 
 namespace RizzyUI;
 
 public class RizzyComponent : ComponentBase
 {
-	protected IReadOnlyDictionary<string, object>? attributes;
-	protected string classes = "";
+	protected virtual string RootClass { get; set; } = string.Empty;
 
-	[Parameter(CaptureUnmatchedValues = true)]
-	public IReadOnlyDictionary<string, object> AdditionalAttributes { get; set; } = new Dictionary<string, object>();
+    [Inject] protected TwMerge TwMerge { get; set; } = default!;
 
-	[Parameter]
+    [SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "False positive. This is a parameter.")]
+    [Parameter(CaptureUnmatchedValues = true)]
+    public IDictionary<string, object>? AdditionalAttributes { get; set; } = new Dictionary<string, object>();
+
+    [Parameter]
 	public RenderFragment? ChildContent { get; set; } = default!;
 
 	protected override void OnParametersSet()
 	{
 		base.OnParametersSet();
 
-		classes = $"{AdditionalAttributes.GetValueOrDefault("class")}";
+		if (AdditionalAttributes?.TryGetValue("class", out object? userClasses) == true)
+		{
+			RootClass = TwMerge.Merge(RootClass, userClasses.ToString()) ?? string.Empty;
 
-		attributes = AdditionalAttributes
-			.Where(x => x.Key != "class")
-			.ToDictionary(k => k.Key, v => v.Value);
+			AdditionalAttributes.Remove("class");
+		}
 	}
 }
