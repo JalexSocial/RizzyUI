3bb582e80157dcf38d21fc2f49c0f35081203523
Michael Tanczos <tanczosm@eastonsd.org>
2024-12-03 09:15:01 -0500

    refactor(RizzyUI): enhance Tabs component accessibility and code documentation
    This commit improves the accessibility of the Tabs component in RizzyUI by adding keyboard navigation support and improving ARIA roles. It also updates the selected tab name from "Activity" to "activity" in Home.razor under RizzyUI.Docs.
    
    In addition, this commit enhances the code documentation for Tab.razor and TabPanel.razor components, providing more detailed descriptions for properties and methods, as well as clarifying exceptions thrown during initialization.

---
+++ src/RizzyUI/Utility/IdGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RizzyUI.Utility;
+
+public static class IdGenerator
+{
+    public static string UniqueId(string prefix)
+    {
+        return string.Empty;
+    }
+}
