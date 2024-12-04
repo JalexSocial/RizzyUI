8bcfcdd8b33450d2fb9ab2c26f06113c473eb768
Michael Tanczos <mike@logic-gate.com>
2024-12-03 19:11:37 -0500

    feat: enhance documentation and UI components
    This commit introduces several enhancements to the RizzyUI library. The SidebarLinkItem.Icon documentation has been updated to reflect changes in the Icon property type. Additional content sections have been added to the Home page, providing a more comprehensive overview of the library's features.
    
    The Heading component now registers itself with its parent QuickReferenceContainer only once, preventing potential duplication issues. This change is facilitated by a new private field `_registered`.
    
    New CSS classes for margin-left properties have also been added, offering more flexibility in layout design.
    
    Lastly, two new constants have been introduced in Constants.cs for package name and content URL resolution, improving code readability and maintainability.

--- src/RizzyUI/Constants.cs
+++ src/RizzyUI/Constants.cs
@@ -8,6 +8,15 @@ namespace RizzyUI;
 
 internal class Constants
 {
+    /// <summary>
+    /// Package name
+    /// </summary>
     public const string PackageName = "RizzyUI";
+
+    /// <summary>
+    /// Resolves path to internal package assets
+    /// </summary>
+    /// <param name="path"></param>
+    /// <returns></returns>
     public static string ContentUrl(string path) => $"/_content/{PackageName}/{path}";
 }
