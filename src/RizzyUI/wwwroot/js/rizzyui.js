8924c56cd9572e5e727cb60c660b11008ce42335
Michael Tanczos <tanczosm@eastonsd.org>
2024-12-12 09:44:17 -0500

    refactor(CodeViewer): simplify code and improve readability
    This commit refactors the CodeViewer component in RizzyUI. It simplifies the code by moving logic into a separate `rzCodeViewer` object, improving readability and maintainability. The changes also include renaming some variables for better clarity, such as changing `copyHTML()` to `copyHTML`, and `expand=!expand` to `toggleExpand`.

--- src/RizzyUI/wwwroot/js/rizzyui.js
+++ src/RizzyUI/wwwroot/js/rizzyui.js
@@ -8,26 +8,91 @@ document.addEventListener('alpine:init', () => {
 
                 dismiss() {
                     this.showAlert = false;
-                },
+                }
             }
         });
 
-    Alpine.data('rzBrowser', () => {
-        return {
-            screenSize: '',
+    Alpine.data('rzBrowser',
+        () => {
+            return {
+                screenSize: '',
 
-            setDesktopScreenSize() {
-                this.screenSize = '';
-            },
+                setDesktopScreenSize() {
+                    this.screenSize = '';
+                },
 
-            setTabletScreenSize() {
-                this.screenSize = 'max-w-2xl';
-            },
+                setTabletScreenSize() {
+                    this.screenSize = 'max-w-2xl';
+                },
 
-            setPhoneScreenSize() {
-                this.screenSize = 'max-w-sm';
-            },
+                setPhoneScreenSize() {
+                    this.screenSize = 'max-w-sm';
+                }
 
-        }
-    })
+            }
+        });
+
+    Alpine.data('rzCodeViewer',
+        () => {
+            return {
+                expand: false,
+                border: true,
+                codeStyle: '',
+                copied: false,
+                notCopied() {
+                    return !this.copied;
+                },
+                toggleCopied() {
+                    this.copied = !this.copied;
+                },
+                toggleExpand() {
+                    this.expand = !this.expand;
+                },
+                copyHTML() {
+                    navigator.clipboard.writeText(this.$refs.codeBlock.textContent);
+                    this.copied = !this.copied;
+                },
+                init() {
+                    // Retrieve the assets from the dataset
+                    const assets = JSON.parse(this.$el.dataset.assets);
+                    const codeId = this.$el.dataset.codeid;
+
+                    loadjs(assets, {
+                        success: () => {
+                            // Ensure Highlight.js styles are applied after the script loads
+                            const codeBlock = document.getElementById(codeId);
+
+                            if (window.hljs && codeBlock) {
+                                window.hljs.highlightElement(codeBlock);
+                            }
+                        },
+                        error: () => {
+                            console.error('Failed to load Highlight.js');
+                        },
+                        async: false
+                    });
+                }
+            }
+        });
+
+    Alpine.data('rzQuickReferenceContainer',
+        () => {
+            return {
+                headings: [],
+                currentHeadingId: '',
+                handleHeadingClick() {
+                    const id = this.$el.dataset.headingid;
+                    setTimeout(() => { this.currentHeadingId = id; }, 10);
+                },
+                setCurrentHeading(id) {
+                    if (this.headings.includes(id)) {
+                        this.currentHeadingId = id;
+                    }
+                },
+                init() {
+                    this.headings = JSON.parse(this.$el.dataset.headings);
+                    this.currentHeadingId = this.$el.dataset.currentheadingid;
+                }
+            }
+        });
 })
