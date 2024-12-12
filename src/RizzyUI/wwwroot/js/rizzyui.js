d69b7997dec6737fa8f92b5ef91c231802c2bce5
Michael Tanczos <mike@logic-gate.com>
2024-12-11 19:12:45 -0500

    feat: migrating alpine data to directives

--- src/RizzyUI/wwwroot/js/rizzyui.js
+++ src/RizzyUI/wwwroot/js/rizzyui.js
@@ -1,1 +1,33 @@
 ﻿/* RizzyUI JS */
+
+document.addEventListener('alpine:init', () => {
+    Alpine.data('rzAlert',
+        () => {
+            return {
+                showAlert: true,
+
+                dismiss() {
+                    this.showAlert = false;
+                },
+            }
+        });
+
+    Alpine.data('rzBrowser', () => {
+        return {
+            screenSize: '',
+
+            setDesktopScreenSize() {
+                this.screenSize = '';
+            },
+
+            setTabletScreenSize() {
+                this.screenSize = 'max-w-2xl';
+            },
+
+            setPhoneScreenSize() {
+                this.screenSize = 'max-w-sm';
+            },
+
+        }
+    })
+})
