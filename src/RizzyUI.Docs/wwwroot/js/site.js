f484eb9637888c830ad7775a8054271909f0cdb9
Michael Tanczos <mike@logic-gate.com>
2025-02-19 14:22:14 -0500

    feat: Add site.js script and new Card component
    A new script reference has been added to the App.razor file, enhancing the functionality of the application. Additionally, a new Card component with Tabs has been introduced in Test.razor, providing more interactive options for users.

---
+++ src/RizzyUI.Docs/wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿document.addEventListener('alpine:init', () => {
+
+
+    Alpine.data('demoTest', () => ({
+        tabRef: null,
+        init() {
+            this.tabRef = document.getElementById("tabDemo");
+        },
+        handleButtonClick() {
+            var data = Alpine.$data(this.tabRef); // This is how to get data for an element
+            var data2 = Alpine.mergeProxies(this.tabRef._x_dataStack);
+            var data3 = this.tabRef.$data;
+            console.log(data.buttonRef);
+        },
+        destroy() {
+
+        }
+    }));
+
+});
