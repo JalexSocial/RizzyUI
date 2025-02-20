84e82873e45400769b7270118223d61a4eb5571b
Michael Tanczos <mike@logic-gate.com>
2025-02-20 07:00:33 -0500

    refactor: Simplify button click handler in site.js
    Removed unnecessary data retrieval lines from the handleButtonClick function. This simplifies the code and improves readability without affecting functionality.

--- src/RizzyUI.Docs/wwwroot/js/site.js
+++ src/RizzyUI.Docs/wwwroot/js/site.js
@@ -8,8 +8,7 @@
         },
         handleButtonClick() {
             var data = Alpine.$data(this.tabRef); // This is how to get data for an element
-            var data2 = Alpine.mergeProxies(this.tabRef._x_dataStack);
-            var data3 = this.tabRef.$data;
+
             console.log(data.buttonRef);
         },
         destroy() {
