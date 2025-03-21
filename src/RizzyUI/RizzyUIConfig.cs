2e75279d02d5cf3aab1a6c40c10e75d6bfecff1d
Michael Tanczos <mike@logic-gate.com>
2025-03-21 14:34:04 -0400

    refactor: Remove Nonce HMAC Key properties
    The Nonce HMAC Key properties, which were previously used for signing generated nonce values, have been removed from the RizzyUIConfig class. This includes both the base-64 encoded string and its decoded byte array representation.

--- src/RizzyUI/RizzyUIConfig.cs
+++ src/RizzyUI/RizzyUIConfig.cs
@@ -9,17 +9,4 @@ public sealed class RizzyUIConfig
     /// Default theme for RizzyUI (defaults to ArcticTheme)
     /// </summary>
     public RizzyTheme DefaultTheme { get; set; } = RizzyTheme.Default;
-
-    /// <summary>
-    /// Gets or sets the Nonce HMAC Key as a base-64 encoded string. This key is used to sign generated nonce values
-    /// such that they may be revalidated when using htmx.
-    /// </summary>
-    // ReSharper disable once InconsistentNaming
-    public string NonceHMACKey { get; set; } = string.Empty;
-
-    /// <summary>
-    /// Gets the decoded Nonce HMAC Key as a byte array.
-    /// </summary>
-    // ReSharper disable once InconsistentNaming
-    public byte[] NonceHMACKeyBytes => Convert.FromBase64String(NonceHMACKey);
 }
