61735847571cfc0233b4f622879df57edb767f73
Michael Tanczos <mike@logic-gate.com>
2025-03-24 21:45:12 -0400

    refactor(build): update webpack config and project structure
    - Removed unnecessary folder inclusion in RizzyUI.csproj.
    - Updated output path in webpack.config.js for 'rizzyui-csp.js'.
    - Added a new production build configuration for 'rizzyui-csp.min.js' with Babel transpilation.
    - Renamed existing JavaScript files to reflect the updated directory structure.

--- src/RizzyUI/webpack.config.js
+++ src/RizzyUI/webpack.config.js
@@ -44,7 +44,7 @@ module.exports = [
         entry: path.resolve(__dirname, 'wwwroot/js/rizzyui-csp.js'),
         output: {
             filename: 'rizzyui-csp.js',
-            path: path.resolve(__dirname, 'wwwroot/dist/rizzyui'),
+            path: path.resolve(__dirname, 'wwwroot/dist/'),
         },
         mode: 'development', // Enables unminified output
         devtool: 'source-map', // Optional: Generates source maps for easier debugging
@@ -69,5 +69,36 @@ module.exports = [
         experiments: {
             outputModule: true,
         }
-    }
+    },
+    {
+        name: 'rizzyui-csp',
+        entry: path.resolve(__dirname, 'wwwroot/js/rizzyui-csp.js'),
+        output: {
+            filename: 'rizzyui-csp.min.js',
+            path: path.resolve(__dirname, 'wwwroot/dist/'),
+        },
+        mode: 'production', // Enables unminified output
+        devtool: 'source-map', // Optional: Generates source maps for easier debugging
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
+                    }
+                }
+            ]
+        },
+        resolve: {
+            extensions: ['.js'], // Automatically resolve these extensions
+            modules: ['node_modules'], // Allow imports from node_modules
+        },
+        experiments: {
+            outputModule: true,
+        }
+    }    
 ];
