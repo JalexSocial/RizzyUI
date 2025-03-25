b41ed3828c5f734dcf3a8f9b03b9382104910edb
Michael Tanczos <tanczosm@eastonsd.org>
2025-03-25 08:16:54 -0400

    `refactor(RizzyUI): streamline webpack configuration and add babel-loader`
    This commit simplifies the webpack configuration for RizzyUI by removing redundant entries and adding a new loader, 'babel-loader', to transpile JavaScript files. The changes also include automatic resolution of '.js' extensions and allowing imports from 'node_modules'. The output paths have been updated to ensure consistency across different configurations.

--- src/RizzyUI/webpack.config.js
+++ src/RizzyUI/webpack.config.js
@@ -3,42 +3,68 @@
 const path = require('path');
 
 module.exports = [
-    /*
-    // Unminified Bundle Configuration
     {
-        name: 'unminified',
-        entry: [
-            path.resolve(__dirname, 'wwwroot/js/rizzy-streaming.js'),
-            path.resolve(__dirname, 'wwwroot/js/rizzy-nonce.js'),
-            path.resolve(__dirname, 'wwwroot/js/rizzyui.js'),
-            path.resolve(__dirname, 'wwwroot/js/vendor/loadjs/loadjs.js')
-        ],
+        name: 'rizzyui',
+        entry: path.resolve(__dirname, 'wwwroot/js/rizzyui.js'),
         output: {
             filename: 'rizzyui.js',
-            path: path.resolve(__dirname, 'wwwroot/dist'),
+            path: path.resolve(__dirname, 'wwwroot/dist/'),
         },
         mode: 'development', // Enables unminified output
         devtool: 'source-map', // Optional: Generates source maps for easier debugging
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
+                    }
+                }
+            ]
+        },
+        resolve: {
+            extensions: ['.js'], // Automatically resolve these extensions
+            modules: ['node_modules'], // Allow imports from node_modules
+        },
+        experiments: {
+            outputModule: true,
+        }
     },
-
-    // Minified Bundle Configuration
     {
-        name: 'minified',
-        entry: [
-            path.resolve(__dirname, 'wwwroot/js/rizzy-streaming.js'),
-            path.resolve(__dirname, 'wwwroot/js/rizzy-nonce.js'),
-            path.resolve(__dirname, 'wwwroot/js/rizzyui.js'),
-            path.resolve(__dirname, 'wwwroot/js/vendor/loadjs/loadjs.js')
-        ],
+        name: 'rizzyui-min',
+        entry: path.resolve(__dirname, 'wwwroot/js/rizzyui.js'),
         output: {
             filename: 'rizzyui.min.js',
-            path: path.resolve(__dirname, 'wwwroot/dist'),
+            path: path.resolve(__dirname, 'wwwroot/dist/'),
         },
-        mode: 'production', // Enables minification and optimizations
-        // Source maps are optional for minified bundle
-        // devtool: 'source-map',
+        mode: 'production', // Enables unminified output
+        //devtool: 'source-map', // Optional: Generates source maps for easier debugging
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
+                    }
+                }
+            ]
+        },
+        resolve: {
+            extensions: ['.js'], // Automatically resolve these extensions
+            modules: ['node_modules'], // Allow imports from node_modules
+        },
+        experiments: {
+            outputModule: true,
+        }
     },
-    */
     {
         name: 'rizzyui-csp',
         entry: path.resolve(__dirname, 'wwwroot/js/rizzyui-csp.js'),
@@ -71,14 +97,14 @@ module.exports = [
         }
     },
     {
-        name: 'rizzyui-csp',
+        name: 'rizzyui-csp-min',
         entry: path.resolve(__dirname, 'wwwroot/js/rizzyui-csp.js'),
         output: {
             filename: 'rizzyui-csp.min.js',
             path: path.resolve(__dirname, 'wwwroot/dist/'),
         },
         mode: 'production', // Enables unminified output
-        devtool: 'source-map', // Optional: Generates source maps for easier debugging
+        //devtool: 'source-map', // Optional: Generates source maps for easier debugging
         module: {
             rules: [
                 {
@@ -100,5 +126,5 @@ module.exports = [
         experiments: {
             outputModule: true,
         }
-    }    
+    }
 ];
---
+++ src/RizzyUI/wwwroot/js/rizzyui.js
@@ -0,0 +1,18 @@
+﻿import Alpine from 'alpinejs';
+import collapse from '@alpinejs/collapse';
+import intersect from '@alpinejs/intersect';
+import focus from '@alpinejs/focus';
+import registerComponents from './alpine/components.js';
+
+// Register Alpine.js extensions
+Alpine.plugin(collapse);
+Alpine.plugin(intersect);
+Alpine.plugin(focus)
+registerComponents(Alpine);
+
+window.Alpine = Alpine
+
+Alpine.start()
+
+console.log("Rizzy UI Loaded");
+
