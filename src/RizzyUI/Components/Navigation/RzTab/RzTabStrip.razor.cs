e2d0f31a71b251593abef54e27240ba8a21bff46
Michael Tanczos <mike@logic-gate.com>
2025-10-23 20:42:54 -0400

    refactor(form): streamline data binding
    Remove unnecessary @bind-Value directives for simpler code management
    and improve consistency by using ValueExpression instead.
    
    Relates to mat-tailwind-variants-remove-rizzyui-tv

--- src/RizzyUI/Components/Navigation/RzTab/RzTabStrip.razor.cs
+++ src/RizzyUI/Components/Navigation/RzTab/RzTabStrip.razor.cs
@@ -1,29 +1,15 @@
 
 using Microsoft.AspNetCore.Components;
+using RizzyUI.Extensions;
 using TailwindVariants.NET;
 
 namespace RizzyUI;
 
-/// <summary>
-/// Defines styling properties for a TabStrip component.
-/// </summary>
 public interface IHasTabStripStylingProperties
 {
-    /// <summary>
-    /// Gets the color of the underline for the selected tab.
-    /// </summary>
     public SemanticColor SelectedTabUnderlineColor { get; }
-    /// <summary>
-    /// Gets the justification of the tabs within the strip.
-    /// </summary>
     public Justify Justify { get; }
-    /// <summary>
-    /// Gets the spacing between tabs.
-    /// </summary>
     public Size SpaceBetween { get; }
-    /// <summary>
-    /// Gets the total number of tabs.
-    /// </summary>
     public int TabCount { get; }
 }
 
@@ -37,35 +23,18 @@ public partial class RzTabStrip : RzComponent<RzTabStripSlots>, IHasTabStripStyl
     [CascadingParameter]
     private RzTabs? Parent { get; set; }
 
-    /// <summary>
-    /// Gets or sets the horizontal alignment of tab content within the strip.
-    /// </summary>
     [Parameter]
     public Justify Justify { get; set; } = Justify.Center;
 
-    /// <summary>
-    /// Gets or sets the gap spacing between tabs.
-    /// </summary>
     [Parameter]
     public Size SpaceBetween { get; set; } = Size.Medium;
 
-    /// <summary>
-    /// Gets or sets the child content, which should be a series of <see cref="RzTab"/> components.
-    /// </summary>
     [Parameter]
     public RenderFragment? ChildContent { get; set; }
 
-    /// <summary>
-    /// Gets the color of the underline for the selected tab.
-    /// </summary>
     public SemanticColor SelectedTabUnderlineColor => Parent?.SelectedTabUnderlineColor ?? SemanticColor.Primary;
-
-    /// <summary>
-    /// Gets the total number of tabs in the strip.
-    /// </summary>
     public int TabCount => Parent?._tabs.Count ?? 0;
 
-    /// <inheritdoc/>
     protected override void OnInitialized()
     {
         base.OnInitialized();
@@ -74,6 +43,5 @@ public partial class RzTabStrip : RzComponent<RzTabStripSlots>, IHasTabStripStyl
             throw new InvalidOperationException($"{GetType()} must exist within an RzTabs component.");
     }
 
-    /// <inheritdoc/>
     protected override TvDescriptor<RzComponent<RzTabStripSlots>, RzTabStripSlots> GetDescriptor() => Theme.RzTabStrip;
 }
